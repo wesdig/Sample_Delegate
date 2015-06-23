bc6aef270c0329800de5112b8d8cc523298fa349
wesdig <143829@supinfo.com>
2015-06-23 12:03:08 +0200

    [Initialisation]
    Ajout des fichiers source de base

---
+++ AppliTesting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mylist = new List<string> { "fabrice", "maxime", "alexandre", "fabien", "bernard", "yannick", "samuel" };
+
+            DisplayFilter(mylist, IsFabrice);
+            DisplayFilter(mylist, ContainsFab);
+            DisplayFilter(mylist, m => m.EndsWith("n"));
+            Console.ReadLine();
+        }
+
+        static void DisplayFilter(List<string> persons, FilterList filter)
+        {
+            for (int i = 0; i < persons.Count; i++)
+            {
+                if (filter(persons[i]))
+                {
+                    Console.WriteLine("Résultat : {0} / Index : {1}", persons[i], i);
+                }
+            }
+        }
+
+        public delegate bool FilterList(string list);
+
+        static bool IsFabrice(string person)
+        {
+            return person == "fabrice";
+        }
+
+        static bool ContainsFab(string person)
+        {
+            return person.ToLower().Contains("fab");
+        }
+    }
+}
